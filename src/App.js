e919c5d44972887b6945b5d53c9b6ae9bc56b777
Dogukandinler <82346619+Dogukandinler@users.noreply.github.com>
2022-05-28 16:15:03 +0300

    app.js add

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import NewRecords from './components/NewRecords/NewRecords';
 function App() {
 
     const checks = [
-        { id: "1",company:"", date: new Date(2022, 1, 6),  explanation:"Orospu cocuklarına hayır parası" ,debt:100, claim:0,debtBlance:100,claimBlance:0},
-        { id: "2",company:"", date: new Date(2022, 1, 12), explanation:"Yemek" ,debt:300, claim:0,debtBlance:400,claimBlance:0},
-        { id: "3",company:"", date: new Date(2022, 2, 13), explanation:"Alışveriş" ,debt:100, claim:0,debtBlance:500,claimBlance:0},
-        { id: "4",company:"", date: new Date(2022, 2, 24), explanation:"Ödeme" ,debt:300, claim:0,debtBlance:800,claimBlance:0},
-        { id: "5",company:"", date: new Date(2022, 3, 11), explanation:"Satış" ,debt:0, claim:100,debtBlance:800,claimBlance:100},
-        { id: "6",company:"", date: new Date(2022, 3, 28), explanation:"Satış" ,debt:0, claim:500,debtBlance:800,claimBlance:600},
+        { id: "1",company:"", date: new Date(2022, 1, 6),  explanation:"Orospu cocuklarına hayır parası" ,debt:100, claim:"",debtBlance:100,claimBlance:0},
+        { id: "2",company:"", date: new Date(2022, 1, 12), explanation:"Yemek" ,debt:300, claim:"",debtBlance:400,claimBlance:0},
+        { id: "3",company:"", date: new Date(2022, 2, 13), explanation:"Alışveriş" ,debt:100, claim:"",debtBlance:500,claimBlance:0},
+        { id: "4",company:"", date: new Date(2022, 2, 24), explanation:"Ödeme" ,debt:300, claim:"",debtBlance:800,claimBlance:0},
+        { id: "5",company:"", date: new Date(2022, 3, 11), explanation:"Satış" ,debt:"", claim:100,debtBlance:800,claimBlance:100},
+        { id: "6",company:"", date: new Date(2022, 3, 28), explanation:"Satış" ,debt:"", claim:500,debtBlance:800,claimBlance:600},
  
 ];  
 
+    const addRecordHandler = (record) =>{
+        console.log(record)
+    }
     return(
 
         <div className="div-1">
-            <NewRecords></NewRecords>
+            <NewRecords onAddRecord={addRecordHandler}></NewRecords> 
             <Title></Title>
             <Records items ={checks}/>
         </div>
--- src/components/NewRecords/RecordsForm.js
+++ src/components/NewRecords/RecordsForm.js
@@ -1,66 +1,63 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
+import Records from "../Records";
 import "./RecordsForm.css";
 
-const RecordsForm = () => {
+const RecordsForm = (props) => {
+  const [enteredExplanation, setEnteredExplanation] = useState("");
+  const [enteredAmounth, setEnteredAmounth] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
+  const explanationChangeHandler = (event) => {
+    setEnteredExplanation(event.target.value);
+  };
+  const amounthChangeHandler = (event) => {
+    setEnteredAmounth(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredExplanation);
+    console.log(enteredAmounth);
+    console.log(enteredDate);
 
-
-  /*
-  const[enteredExplanation,setEnteredExplanation]=useState("");
-  const[enteredAmounth,setEnteredAmounth]=useState("");
-  const[enteredDate,setEnteredDate]=useState("");
-  */
-
-  const [userInput,setUserInput] = useState(
-    {
-      enteredExplanation :'',
-      enteredAmounth :'',
-      enteredDate :'',
-     }
-  );
-  
-  const explanationChangeHandler = (event) =>{
-    setUserInput((prevState) =>{
-      return{
-      ...prevState,
-      enteredExplanation : event.target.value,
+    const recordData={
+      explanation: enteredExplanation,
+      amounth : enteredAmounth,
+      date : enteredDate
     }
-    }); 
-  };
-  const amounthChangeHandler = (event) =>{
-    setUserInput((prevState) =>{
-      return{
-      ...prevState,
-      enteredAmounth : event.target.value,
-    } 
-    }); 
-  };
-  const dateChangeHandler = (event) =>{
-    setUserInput((prevState) =>{
-      return{
-      ...prevState,
-      enteredDate : event.target.value,
-    }
-    }); 
-  };
-  const submitHandler = (event) =>{
-     event.preventDefault();
-     console.log(userInput) ;
 
-  }
-   
+    props.onSaveRecordData(recordData)
+
+    setEnteredAmounth("");
+    setEnteredDate("");
+    setEnteredExplanation("");
+  };
+
   return (
-    <form onSubmit={submitHandler}> 
+    <form onSubmit={submitHandler}>
       <div className="Form--items">
         <div className="Form-Date">
-          <input type="date" onChange={dateChangeHandler} min="2015-01-01"></input>
+          <input
+            type="date"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+            min="2015-01-01"
+          ></input>
         </div>
         <div className="Form-Açıklama">
-          <input placeholder="Açıklama Giriniz" onChange={explanationChangeHandler} type="text"></input>
+          <input
+            placeholder="Açıklama Giriniz"
+            value={enteredExplanation}
+            onChange={explanationChangeHandler}
+            type="text"
+          ></input>
         </div>
         <div className="Form-Tutar">
           <input
-            placeholder="Tutarı Giriniz"  
+            placeholder="Tutarı Giriniz"
+            value={enteredAmounth}
             type="number"
             min="1"
             max="99999"
