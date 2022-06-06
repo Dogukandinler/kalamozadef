e4b221a39b5fd7045dd4aa940037384bcdafa9ed
Dogukandinler <82346619+Dogukandinler@users.noreply.github.com>
2022-06-06 18:32:16 +0300

    son hali

--- src/components/Kalamoza/KalamozaItem.js
+++ src/components/Kalamoza/KalamozaItem.js
@@ -5,15 +5,37 @@ import Card from "../UI/Card";
 
 const KalamozaItem = (props) => {
   
+  const [claimBlanceAll,AlacakSet] = useState(0)
+  const [debtBlanceAll,BorcSet] = useState(0)
   const [isClaim, setIsClaim] = useState(false);
 
+  const alacakBorcekle  = () => {
+    if (props.selection === "Alacak"){
+        BorcSet(props.debtclaim) }
+
+    else {
+      AlacakSet(props.debtclaim)}
+
+  }
   useEffect(() => {
-    const onClickHeader = () => {
+    const onBackgroundColor = () => {
       if (props.selection === "Alacak") setIsClaim(!isClaim);
       else setIsClaim(isClaim);
     };
-    onClickHeader();
+   onBackgroundColor();
+   alacakBorcekle();
+
   }, []);
+  const Ekleee =() =>{
+
+    const bData={
+      borc : claimBlanceAll,
+      alacak: debtBlanceAll,
+    }
+  
+    props.ustetası(bData)
+
+  };
 
   return (
     <Card>
@@ -23,8 +45,9 @@ const KalamozaItem = (props) => {
         </div>
         <div className="Açıklama">{props.explanation}</div>
         <div className="Borç">{props.debtclaim}₺</div>
-        <div className="Borçbakiye">{props.debtBlance}₺</div>
-        <div className="Alacakbakiye">{props.claimBlance}₺</div>
+        <div className="Borçbakiye">{claimBlanceAll}₺</div>
+        <div className="Alacakbakiye">{debtBlanceAll}₺</div>
+        <button onClick={Ekleee}>Ekle</button>
       </div>
     </Card>
   );
--- src/components/Kalamoza/KalamozaList.js
+++ src/components/Kalamoza/KalamozaList.js
@@ -1,52 +1,51 @@
-import React, { useState,useEffect} from "react";
+import React  from "react";
 import KalamozaItem from "./KalamozaItem";
 import "./KalamozaList.css";
 
 const KalamozaList = (props) => {
-  
   const sortByDate = (a, b) => {
     return a.date - b.date;
+    
   };
 
   if (props.items.length === 0) {
     return <p className="p">No Records found</p>;
   }
 
-  function alacakYap() {
-    let araDegerAlacak = 0;
-    let alacakDizi = [];
-
-    for (let i = 0; i < props.items.length; i++) {
-      if (props.items[i].selection === "Alacak") {
-        araDegerAlacak = araDegerAlacak + props.items[i].debtclaim;
-        alacakDizi.push(araDegerAlacak);
-          console.log(alacakDizi)
-      }
-    }
-    return alacakDizi;
+  const toplamborceklehandler =(addedust,borcas) =>{
+   let bData ={...addedust}; 
+   console.log(bData)
   }
 
-  function BorcYap() {
-    let araDegerBorc = 0;
-    let borcDizi = [];
-
-    for (let i = 0; i < props.items.length; i++) {
-      if (props.items[i].selection === "Borç") {
-        araDegerBorc = araDegerBorc + props.items[i].debtclaim;
-        borcDizi.push(araDegerBorc);
-          console.log(borcDizi)
-      }
-    }
-    return borcDizi;
-  }
-  console.log(BorcYap())
+  // function islemYap() {
+  //   let araDegerAlacak = 0;
+  //   let araDegerBorc = 0;
+  //   let alacakDizi = [];
+  //   let borcDizi = [];
 
 
+  //   for (let i = 0; i < props.items.length; i++) {
+  //     if (props.items[i].selection === "Alacak") {
+  //       araDegerAlacak = araDegerAlacak + props.items[i].debtclaim;
+  //       alacakDizi.push(araDegerAlacak);
+          
+  //     } else {
+  //       araDegerBorc = araDegerBorc + props.items[i].debtclaim;
+  //       borcDizi.push(araDegerBorc);
+        
+  //     }
+  //   }
+  //   return [alacakDizi,borcDizi];
+    
+  // }
+
+  // console.log(islemYap());
 
   return (
     <ul className="checks-list">
       {props.items.sort(sortByDate).map((checks) => (
         <KalamozaItem
+          ustetası={toplamborceklehandler}
           key={checks.id}
           date={checks.date}
           explanation={checks.explanation}
